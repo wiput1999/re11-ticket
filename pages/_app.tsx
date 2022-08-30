2601d5bf864ebbe1439b64f3b5daf3ea063341f7
Wiput Pootong <wiput.pootong@gmail.com>
2022-08-30 23:34:51 +0700

    feat: graphql api

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Component {...pageProps}>
+      <Head>
+        <title>YWC Reunion 11</title>
+      </Head>
+    </Component>
+  )
 }
 
 export default MyApp
---
+++ pages/api/graphql.ts
@@ -0,0 +1,53 @@
+import { gql, ApolloServer } from 'apollo-server-micro'
+import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core'
+import { NextApiRequest, NextApiResponse } from 'next'
+
+let books = [
+  {
+    id: 0,
+    name: 'JavaScript for Dummies',
+  },
+  // ... Write a few more ...
+]
+
+const typeDefs = gql`
+  type Book {
+    id: ID!
+    name: String
+  }
+  type Query {
+    getBooks: [Book]
+  }
+`
+
+const resolvers = {
+  Query: {
+    getBooks: () => {
+      return books
+    },
+  },
+}
+
+const apolloServer = new ApolloServer({
+  typeDefs,
+  resolvers,
+  plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
+})
+
+const startServer = apolloServer.start()
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  await startServer
+  await apolloServer.createHandler({
+    path: '/api/graphql',
+  })(req, res)
+}
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+}
